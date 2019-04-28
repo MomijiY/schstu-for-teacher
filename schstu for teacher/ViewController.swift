0ea3c0d1a1c7c774bfce5ccfcf1cdd1056d15b99
吉川椛 <momiji@yoshikawanomacbook-pro.local>
2019-04-28 16:14:13 +0900

    Initial Commit

---
+++ schstu for teacher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  schstu for teacher
+//
+//  Created by 吉川椛 on 2019/04/28.
+//  Copyright © 2019 吉川椛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
