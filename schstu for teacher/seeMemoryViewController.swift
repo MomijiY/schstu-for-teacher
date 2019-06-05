4d6d33deb1f28b2f032c9fffd37eb454b3e30e12
吉川椛 <js20171059@yamanashi-eiwa.ac.jp>
2019-06-05 19:59:23 +0900

    生徒の記録を見る画面を付け足しました。 生徒が提出した記録が反映されるようにしたいです。

---
+++ schstu for teacher/seeMemoryViewController.swift
@@ -0,0 +1,59 @@
+//
+//  seeMemoryViewController.swift
+//  schstu for teacher
+//
+//  Created by 吉川椛 on 2019/06/05.
+//  Copyright © 2019 吉川椛. All rights reserved.
+//
+
+import UIKit
+
+class seeMemoryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleTextField.text = saveData.object(forKey: "title") as? String
+        contentTextView.text = saveData.object(forKey: "content") as? String
+        titleTextField.delegate = self
+        
+    }
+    
+    @IBAction func MemorySave() {
+        saveData.set(titleTextField.text, forKey: "title")
+        saveData.set(contentTextView.text, forKey: "content")
+        
+        let alert: UIAlertController = UIAlertController(title: "OK", message: "メモの保存が完了しました", preferredStyle: .alert)
+        
+        alert.addAction(
+            UIAlertAction(title: "OK", style: .default, handler: { action in
+                
+                self.navigationController?.popViewController(animated: true)
+            }
+            )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return  true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
